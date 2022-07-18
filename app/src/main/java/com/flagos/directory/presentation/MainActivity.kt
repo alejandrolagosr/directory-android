629e52cb4eb017a78075e80a9a6b72f40288c1ca
Alejandro Lagos <flagos.rd@gmail.com>
2022-07-18 14:02:14 -0500

    Added screens for error and empty lists
    Signed-off-by: Alejandro Lagos <flagos.rd@gmail.com>

--- app/src/main/java/com/flagos/directory/presentation/MainActivity.kt
+++ app/src/main/java/com/flagos/directory/presentation/MainActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import androidx.activity.viewModels
+import com.flagos.directory.R
 import com.flagos.directory.databinding.ActivityMainBinding
+import com.flagos.directory.presentation.MainViewModel.EmployeeDirectoryState
+import com.flagos.directory.presentation.MainViewModel.EmployeeDirectoryState.*
 import com.flagos.directory.presentation.adapter.EmployeeAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,15 +25,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.recycler.adapter = adapter
-        viewModel.fetchEmployees()
 
-        observeLiveData()
+        with(viewModel) {
+            fetchEmployees()
+            onEmployeeDirectoryState.observe(this@MainActivity) { state -> setUiState(state) }
+        }
     }
 
-    private fun observeLiveData() {
-        with(viewModel) {
-            onEmployeeDirectoryListRetrieved.observe(this@MainActivity) { adapter.submitList(it) }
-            onLoading.observe(this@MainActivity) { loading -> binding.progress.visibility = if (loading) VISIBLE else GONE }
+    private fun setUiState(state: EmployeeDirectoryState) {
+        when(state) {
+            OnEmptyList -> setEmptyState()
+            is OnError -> setErrorState(state.error)
+            is OnListRetrieved -> adapter.submitList(state.list)
+            is OnLoading -> setLoadingState(state.loading)
+        }
+    }
+
+    private fun setLoadingState(loading: Boolean) {
+        if (loading) {
+            binding.progress.visibility = VISIBLE
+            binding.textFailedMessage.visibility = GONE
+        } else {
+            binding.progress.visibility = GONE
+        }
+    }
+
+    private fun setErrorState(error: String?) {
+        with(binding.textFailedMessage) {
+            text =  String.format(getString(R.string.text_error), error)
+            setCompoundDrawablesRelativeWithIntrinsicBounds(0, R.drawable.ic_error, 0, 0)
+            visibility = VISIBLE
+        }
+    }
+
+    private fun setEmptyState() {
+        with(binding.textFailedMessage) {
+            text =  getString(R.string.text_empty)
+            setCompoundDrawablesRelativeWithIntrinsicBounds(0, R.drawable.ic_empty, 0, 0)
+            visibility = VISIBLE
         }
     }
 }
--- app/src/main/java/com/flagos/directory/presentation/MainViewModel.kt
+++ app/src/main/java/com/flagos/directory/presentation/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.flagos.directory.data.EmployeeDirectoryRepository
 import com.flagos.directory.domain.model.EmployeeItem
+import com.flagos.directory.presentation.MainViewModel.EmployeeDirectoryState.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
@@ -22,17 +23,27 @@ class MainViewModel @Inject constructor(private val employeeDirectoryRepository:
     private var _onError = MutableLiveData<String>()
     val onError get() = _onError
 
+    private var _onEmployeeDirectoryState = MutableLiveData<EmployeeDirectoryState>()
+    val onEmployeeDirectoryState get() = _onEmployeeDirectoryState
+
     fun fetchEmployees() {
             val employees = employeeDirectoryRepository.fetchEmployeeDirectoryList(
-                onStart = { _onLoading.postValue(true) },
-                onComplete = { _onLoading.postValue(false) },
-                onError = { onError.postValue(it) }
+                onStart = { _onEmployeeDirectoryState.postValue(OnLoading(true)) },
+                onComplete = { _onEmployeeDirectoryState.postValue(OnLoading(false)) },
+                onError = { _onEmployeeDirectoryState.postValue(OnError(it)) }
             )
 
         viewModelScope.launch {
-            employees.distinctUntilChanged().collect {
-                _onEmployeeDirectoryListRetrieved.value = it
+            employees.distinctUntilChanged().collect { list ->
+                _onEmployeeDirectoryState.value = if (list.isNotEmpty()) OnListRetrieved(list) else OnEmptyList
             }
         }
+    }
+
+    sealed class EmployeeDirectoryState {
+        data class OnLoading(val loading: Boolean) : EmployeeDirectoryState()
+        data class OnError(val error: String?) : EmployeeDirectoryState()
+        data class OnListRetrieved(val list : List<EmployeeItem>): EmployeeDirectoryState()
+        object OnEmptyList : EmployeeDirectoryState()
     }
 }
